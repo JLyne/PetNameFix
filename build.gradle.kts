424dfb1b83c4f0d5f77166e8afb7da8fb06199ca
James Lyne <jim+github@not-null.co.uk>
2025-03-14 16:10:51 +0000

    Update gradle, plugins. Refactor gradle scripts
    - Switch to Kotlin
    - Switch to plugin-yml gradle plugin
    - Switch to using version catalog for dependencies
    - Use java toolchain

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    java
+    alias(libs.plugins.pluginYmlPaper)
+    alias(libs.plugins.paperweightUserdev)
+}
+
+group = "io.github.tanguygab"
+version = "1.0.2"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+}
+
+dependencies {
+	compileOnly(libs.paperApi)
+    paperweightDevelopmentBundle(libs.paperweightBundle)
+}
+
+paper {
+    main = "io.github.tanguygab.petnamefix.PetNameFix"
+    apiVersion = libs.versions.paper.get().replace(Regex("\\-R\\d.\\d-SNAPSHOT"), "")
+    authors = listOf("NEZNAMY", "Tanguygab", "Jim (AnEnragedPigeon)")
+    description = "A feature to disable minecraft feature making tamed animals with custom names copy NameTag properties of their owner."
+}
+
+tasks {
+    compileJava {
+        dependsOn(clean)
+        options.compilerArgs.addAll(listOf("-Xlint:all", "-Xlint:-processing"))
+        options.encoding = "UTF-8"
+    }
+}
